2c4eb456a374b9e88a33f5a9c8d6d533ef17544d
suedoh <suedoh36@proton.me>
2023-03-16 21:36:00 -0400

    added no route for handling authentication, started auth handler

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -4,6 +4,16 @@ import (
 	"net/http"
 )
 
+type RequestPayload struct {
+    Action string `json:"action"`
+    Auth AuthPayload `json:"auth,omitempty"`
+}
+
+type AuthPayload struct {
+    Email string `json:"email"`
+    Password string `json:"password"`
+}
+
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request)  {
     payload := jsonResponse{
         Error: false,
@@ -12,3 +22,9 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request)  {
 
     _ = app.writeJSON(w, http.StatusOK, payload)
 }
+
+func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request)  {
+    
+}
+
+
