fe434b554e6bc2b56680c8708d0540b2da8cd123
suedoh <suedoh36@proton.me>
2023-03-19 22:30:50 -0400

    imported mongo drivers, wrote method which will create a connection

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+    webPort = "80"
+    rpcPort = "5001"
+    mongoURL = "mongodb://mongo:27017"
+    grpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Client struct {
+    
+}
+
+func main() {
+    mongoClient, err := connectToMongo()
+    if err != nil {
+        log.Panic(err)
+    }
+
+    client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+    clientOptions := options.Client().ApplyURI(mongoURL)
+    clientOptions.SetAuth(options.Credential{
+        Username: "admin",
+        Password: "password",
+    })
+
+    // mongo connection
+    c, err := mongo.Connect(context.TODO(), clientOptions)
+    if err != nil {
+        log.Println("Error connecting:", err)
+        return nil, err
+    }
+
+    return c, nil
+}
