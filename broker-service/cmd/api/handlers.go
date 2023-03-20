f695308a711d617fc2b593a61c64efdd19d71cb6
suedoh <suedoh36@proton.me>
2023-03-19 22:02:30 -0400

    finished auth service, changed error message

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -67,7 +67,7 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload)  {
         app.errorJSON(w, errors.New("invalid credentials"))
         return
     } else if response.StatusCode != http.StatusAccepted {
-        app.errorJSON(w, errors.New("error calling auth service"))
+        app.errorJSON(w, errors.New("error calling auth service :("))
         return
     }
 
